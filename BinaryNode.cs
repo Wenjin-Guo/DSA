f4864de4d1a99eb8e9c9e2993fc6a872a5f495a5
simon <simonguo.eng@yahoo.com>
2023-12-14 08:12:00 -0500

    add BinaryNode and BinaryTreeLL class

---
+++ BinaryNode.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class BinaryNode
+    {
+        public String? value{get;set;}
+        public BinaryNode? left{get;set;}
+        public BinaryNode? right{get;set;}
+        public int? height{get;set;}
+    }
+}
---
+++ BinaryTreeLL.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class BinaryTreeLL
+    {
+        private BinaryNode root{get;set;}
+        public BinaryNode(){
+            this.root = null;
+        }
+
+    }
+}
