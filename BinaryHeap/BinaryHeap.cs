c4a2571d3c23f5954fc535b3afcf83ce03b451c6
simon <simonguo.eng@yahoo.com>
2024-02-19 09:26:51 -0500

    add peek method

--- BinaryHeap/BinaryHeap.cs
+++ BinaryHeap/BinaryHeap.cs
@@ -7,6 +7,28 @@ namespace DSA
 {
     public class BinaryHeap
     {
-        
+        int[] arr;
+        int sizeOfTree;
+
+        public BinaryHeap(int size){
+            arr=new int[size+1];
+            this.sizeOfTree = 0;
+            Console.WriteLine("Binary Heap has been created");
+        }
+
+        public bool isEmpty(){
+            if(sizeOfTree ==0){
+                return true;
+            }
+            return false;
+        }
+
+        public int? peek(){
+            if(isEmpty()){
+                Console.WriteLine("Binary Heap is empty");
+                return null;
+            }
+            return arr[1];
+        }
     }
 }
